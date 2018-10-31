a27cde7668217b50be38b2d3300389c9ed534967
Kaspar Schleiser <kaspar@schleiser.de>
2018-10-31 16:48:32 +0100

    update-subtrees.py: auto-add if path doesn't exist

--- update-subtrees.py
+++ update-subtrees.py
@@ -8,12 +8,10 @@ import os
 import click
 
 @click.command()
-@click.option("--add", help="add instead of update subtree(s)", is_flag=True)
 @click.option("--chdir", help="cd to dir first", type=str)
 @click.argument("repos", nargs=-1, type=click.STRING)
-def main(add, repos, chdir):
+def main(repos, chdir):
     to_update = set(repos)
-    print(add, repos, chdir, to_update)
 
     if chdir is not None:
         os.chdir(chdir)
@@ -21,17 +19,17 @@ def main(add, repos, chdir):
     with open("subtrees.json", "r") as f:
         subtrees = json.load(f)
 
-    if add:
-        mode = "add"
-    else:
-        mode = "pull"
-
     for entry in subtrees:
         path = entry["path"]
         url = entry["url"]
         branch = entry["branch"]
 
         if len(to_update) == 0 or path in to_update or url in to_update:
+            if not os.path.isdir(path):
+                mode = "add"
+            else:
+                mode = "pull"
+
             try:
                 subprocess.check_call(["git", "subtree", mode, "-m", "updated %s" % path, "-P", path, url, branch])
             except subprocess.CalledProcessError:
